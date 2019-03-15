773bd93d935cc47ffa85c0c3372824b4dd4ad544
Jacek <eviathos@gmail.com>
2019-03-15 20:30:33 +0100

    Gemfile update, added gems, HAML, RSpec
    App converted to HAML syntax, Added testing environment - RSpec, FactoryBot.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,6 @@ Rails.application.configure do
 
   # Production email config
   config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = {
-    api_token: ENV.fetch("POSTMARK_API_KEY")
-  }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
     host: "example.com",
